b75fd99154c72e8cb15aac58b3dd95c2b39f7825
Andreas Evensen <andreas.evensen11@gmail.com>
2020-07-11 15:15:59 +0200

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ setup(
     name = "SacraMathEngine",
     version = "0.0.1",
     author = "Andreas Evensen",
-    author_email = "Andreas Evensen",
+    author_email = "Andreas.evensen11@gmail.com",
     description = "A lightweight 3-d vector package with intents of being used in Sacra Game Engine",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    py_moduels = ["Matrix, Vector, Triangle"],
-    package_dir={'':'src'},
+    py_modules = ["Matrix", "Vector", "Triangle"],
+    #package_dir = {'':'src'},
     packages = find_packages(),
     license = "MIT",
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License"
-    ]    
+    ],
 )
