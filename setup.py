28bf3fc63413e5bb69f07f3452aabc6e67a1606a
Andreas Evensen <andreas.evensen11@gmail.com>
2020-07-13 20:38:40 +0200

    pypi update

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("readme.md", "r") as readme_file:
 
 setup(
     name = "SacraMathEngine",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Andreas Evensen",
     author_email = "Andreas.evensen11@gmail.com",
     description = "A lightweight 3-d vector package with intents of being used in Sacra Game Engine",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    py_modules = ["Matrix", "Vector", "Triangle"],
+    py_modules = ["Matrix", "Vector", "Triangle", "MeshObject"],
     #package_dir = {'':'src'},
     include_package_data = True,
     packages = find_packages(),
